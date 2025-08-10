149543efd527b758c76ede0ef2b6718508e2109a
Ray Chen <ray.chenley.up@gmail.com>
2025-08-10 17:36:22 -0400

    cicd: test

--- CanadaGoose/client/cypress/e2e/home.cy.ts
+++ CanadaGoose/client/cypress/e2e/home.cy.ts
@@ -1,9 +1,10 @@
 describe('Home Page', () => {
   it('loads successfully and shows main content', () => {
     cy.visit('/')
-    cy.contains('SecureAuth').should('exist')
+    cy.contains('CanadaGoose').should('exist')
     cy.contains('Login').should('exist')
     cy.contains('Sign Up').should('exist')
-    cy.contains('Secure Authentication Made Simple').should('exist')
+    cy.contains('Take Control of Your Finances').should('exist')
+    cy.contains('Get Started Free').should('exist')
   })
 })
--- CanadaGoose/client/cypress/e2e/theme.cy.ts
+++ CanadaGoose/client/cypress/e2e/theme.cy.ts
@@ -1,13 +1,28 @@
 describe('Theme Toggle', () => {
   it('toggles between dark and light mode', () => {
     cy.visit('/')
-    cy.get('[aria-label="Toggle theme"]').click()
-    cy.get('html').should(($html) => {
-      expect($html).not.to.have.class('dark')
-    })
-    cy.get('[aria-label="Toggle theme"]').click()
-    cy.get('html').should(($html) => {
-      expect($html).to.have.class('dark')
+
+    // Get the initial theme state
+    cy.get('html').then(($html) => {
+      const isInitiallyDark = $html.hasClass('dark')
+
+      if (isInitiallyDark) {
+        // If initially dark, first click should make it light
+        cy.get('[aria-label="Toggle theme"]').click()
+        cy.get('html').should('not.have.class', 'dark')
+
+        // Second click should make it dark again
+        cy.get('[aria-label="Toggle theme"]').click()
+        cy.get('html').should('have.class', 'dark')
+      } else {
+        // If initially light, first click should make it dark
+        cy.get('[aria-label="Toggle theme"]').click()
+        cy.get('html').should('have.class', 'dark')
+
+        // Second click should make it light again
+        cy.get('[aria-label="Toggle theme"]').click()
+        cy.get('html').should('not.have.class', 'dark')
+      }
     })
   })
 })
